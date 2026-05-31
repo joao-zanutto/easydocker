4af4ed8e8ee373319f6efc5bcb026f606bd07eb3
Joao Zanutto <jpberno@gmail.com>
2026-05-31 12:43:27 -0300

    chore: testing (#97)

---
+++ internal/tui/testhelper_test.go
@@ -0,0 +1,38 @@
+package tui
+
+import (
+	"easydocker/internal/core"
+	"easydocker/internal/tui/shared"
+)
+
+type testModelBuilder struct {
+	m model
+}
+
+func newTestModel() testModelBuilder {
+	m := unwrapModel(New(nil))
+	return testModelBuilder{m: *m}
+}
+
+func (b testModelBuilder) withSize(width, height int) testModelBuilder {
+	b.m.width = width
+	b.m.height = height
+	return b
+}
+
+func (b testModelBuilder) withLoading(loading bool, stage shared.Stage) testModelBuilder {
+	b.m.loading = loading
+	b.m.loadingStage = stage
+	return b
+}
+
+func (b testModelBuilder) withContainers(containers ...core.ContainerRow) testModelBuilder {
+	b.m.browse.Snapshot.Containers = containers
+	b.m.browse.ActiveTab = tabContainers
+	b.m.browse.ShowAll = true
+	return b
+}
+
+func (b testModelBuilder) build() model {
+	return b.m
+}
